6db5fdf1e4835ccf7eafcb0422b9efe53f35af8c
ex-dec <bighadi123@yahoo.com>
2024-04-25 05:42:58 +0700

    fix: deployment build

---
+++ build/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"80bee9e0e6fe3c93985b58d70caa44a3\",\"previewModeSigningKey\":\"50ac12fdc4aed15051f3a8066f957806496574f84339d3930812efb683f2385c\",\"previewModeEncryptionKey\":\"975b050aa65d1c0b77b107f3732d501fe6950a55e0ae7fe66a4b0a2a7c6c6dab\"},\"notFoundRoutes\":[]}"
--- src/constants/URL_ENDPOINT.ts
+++ src/constants/URL_ENDPOINT.ts
@@ -4,9 +4,9 @@
 // const documentEndpoint = "https://dev-api-2023.aquaculturepens.com/cdn/document/";
 // const imageEndpoint = "https://pascasarjana-s3.s3.ap-southeast-1.amazonaws.com/"
 
-const urlEndpoint = "http://host.docker.internal:1993";
+const urlEndpoint = "https://be.aquaculturepens.com";
 const flaskEndpoint = "http://host.docker.internal:8000";
-const imageEndpoint = "http://host.docker.internal:1993/cdn/images/";
-const documentEndpoint = "http://host.docker.internal:1993/cdn/document/";
+const imageEndpoint = "https://be.aquaculturepens.com/cdn/images/";
+const documentEndpoint = "https://be.aquaculturepens.com/cdn/document/";
 
 export { urlEndpoint, imageEndpoint, documentEndpoint };
