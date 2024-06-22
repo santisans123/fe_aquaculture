9d8c194631d57384ba8dc7584dcceaad6e92166a
ex-dec <bighadi123@yahoo.com>
2024-06-22 10:28:17 +0700

    update: build

--- next.config.js
+++ next.config.js
@@ -1,56 +1,29 @@
 const withImages = require("next-images");
-const removeImports = require("next-remove-imports");
-const withTM = require("next-transpile-modules");
-const withPWA = require("next-pwa");
+const removeImports = require("next-remove-imports")();
+const withTM = require("next-transpile-modules")([]);
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: false,
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    disable: false,
-  },
-  transpilePackages: ["next-transpile-modules"],
   i18n: {
     locales: ["id", "en"],
     defaultLocale: "id",
     localeDetection: false,
   },
   images: {
-    disableStaticImages: true,
     domains: [
       "192.168.0.109",
       "localhost",
       "pascasarjana-s3.s3.ap-southeast-1.amazonaws.com",
     ],
+    disableStaticImages: true,
   },
-};
-
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [withPWA, withTM, withImages, removeImports];
-  return plugins.reduce((acc, plugin) => plugin(acc), {
-    ...defaultConfig,
-    ...nextConfig,
-  });
-};
-module.exports = {
   distDir: "build",
 };
 
-/*
-(
-  removeImports(
-    withImages({
-      i18n: {
-        locales: ["id", "en"],
-        defaultLocale: "id",
-        localeDetection: false,
-      },
-      images: {
-        disableStaticImages: true,
-        domains: ["192.168.0.109", "localhost", "pascasarjana-s3.s3.ap-southeast-1.amazonaws.com"],
-      },
-    })
-  )
-);
-*/
+// Menggabungkan semua plugin
+module.exports = withPWA(withTM(withImages(removeImports(nextConfig))));
