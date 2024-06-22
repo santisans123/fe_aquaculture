2610a3d78ef091e575e0031ec62cd1960df38347
santisans123 <santitok21@gmail.com>
2024-06-22 22:49:38 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require("next-pwa")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   i18n: {
     locales: ["id", "en"],
